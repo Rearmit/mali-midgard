d136ba2cdf73db76214bd4f706daa052b8594ef9
rearm <69484060+Rearmit@users.noreply.github.com>
2021-02-09 18:20:40 +0100

    Update mali_kbase_hwaccess_time.h

--- driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_hwaccess_time.h
+++ driver/product/kernel/drivers/gpu/arm/midgard/mali_kbase_hwaccess_time.h
@@ -37,7 +37,12 @@
  *			time in
  */
 void kbase_backend_get_gpu_time(struct kbase_device *kbdev, u64 *cycle_counter,
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(5, 0, 0))
 				u64 *system_time, struct timespec *ts);
+
+#else
+				u64 *system_time, struct timespec64 *ts);
+#endif
 
 /**
  * kbase_wait_write_flush() -  Wait for GPU write flush
